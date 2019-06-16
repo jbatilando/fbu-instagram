be44f0a7ba6bd0d20ec442bde6744cd02f5f2c1e
Miguel Batilando <miguel.batilando@gmail.com>
2019-06-15 19:27:20 -0700

    Added Parse pod

--- Migstagram/AppDelegate.m
+++ Migstagram/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // Initialize Parse server
+    ParseClientConfiguration *config = [ParseClientConfiguration   configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+        
+        configuration.applicationId = @"miguelsinstagram";
+        configuration.server = @"http://miguelsinstagram.herokuapp.com/parse";
+    }];
+    
+    [Parse initializeWithConfiguration:config];
+    
     return YES;
 }
 
